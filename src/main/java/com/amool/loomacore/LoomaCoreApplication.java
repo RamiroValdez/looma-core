bc8db2801ebb65f63d54ad5698402823517cf9e3
RamiroValdez <rvaldez234@alumno.unlam.edu.ar>
2025-09-26 20:11:55 -0300

    Configuracion inicial del proyecto.

---
+++ src/main/java/com/amool/loomacore/LoomaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.amool.loomacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoomaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amool/loomacore/LoomaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amool.loomacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoomaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
