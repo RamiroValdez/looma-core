06ce8f5b7ef96b12e628243a6164152fb089b2f6
Chud50 <pablostadtner@hotmail.com>
2025-10-20 18:03:21 -0300

    Implemente un nuevo endpoint para traer las obras a la vista home desde el back

--- src/main/java/com/amool/application/port/out/WorkPort.java
+++ src/main/java/com/amool/application/port/out/WorkPort.java
@@ -2,10 +2,14 @@ package com.amool.application.port.out;
 
 import com.amool.domain.model.Work;
 
+import java.util.List;
+
 public interface WorkPort {
 
      Long createWork(Work work);
 
      Boolean updateWork(Work work);
+     
+     List<Work> getAllWorks();
 
 }
