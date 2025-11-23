8a8a2a95daa5783b34d047eb9354259f7d0c9c25
RamiroValdez <rvaldez234@alumno.unlam.edu.ar>
2025-11-22 21:15:58 -0300

    Implement chunking for translation in CreateLanguageVersionUseCase and add LangChain dependencies

--- src/main/java/com/amool/application/service/ImagesService.java
+++ src/main/java/com/amool/application/service/ImagesService.java
@@ -6,10 +6,6 @@ import com.amool.domain.model.InMemoryMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Objects;
 import java.util.UUID;
 
